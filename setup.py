e6df9044237816eba217d4b1b545afb6956f7cd9
Sizheng Ma <sma@caltech.edu>
2023-01-13 21:51:30 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+long_description = """\
+To be added
+"""
+
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    from setuptools import setup, find_packages
+
+    setup(
+        name="qnm_filter",
+        version='0.1',
+        description="QNM filters for black hole ringdowns.",
+        long_description=long_description,
+        url="https://github.com/Sizheng-Ma/qnm_filter",
+        author="Sizheng Ma",
+        author_email="sma@caltech.edu",
+        packages=find_packages(),
+        install_requires=[
+            "h5py",
+            "numpy",
+            "qnm",
+            "lalsuite",
+            "matplotlib"
+        ] # TODO: change the order
+    )
