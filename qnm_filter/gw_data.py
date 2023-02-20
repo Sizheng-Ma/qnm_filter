f78e7d4a4f92430f5d90831b3c4cca085032ac39
Sizheng Ma <sma@caltech.edu>
2023-02-20 11:28:14 -0800

    rewrite data_acf

--- qnm_filter/gw_data.py
+++ qnm_filter/gw_data.py
@@ -196,15 +196,6 @@ class Data(pd.Series):
 
         return Data(cond_data, index=cond_time, ifo=self.ifo)
 
-    def data_acf(self, **kws):
-        """Estimate ACFs from data using Welch's method."""
-        dt = self.time_interval
-        fs = self.fft_span
-
-        freq, psd = ss.welch(self.values, fs=fs, nperseg=fs)
-        rho = 0.5*np.fft.irfft(psd) * fs
-        return Data(rho, index=np.arange(len(rho))*dt)
-
     def data_psd(self, **kws):
         """Estimate PSDs from data using Welch's method."""
         fs = self.fft_span
@@ -212,3 +203,12 @@ class Data(pd.Series):
 
         freq, psd = ss.welch(self.values, fs=fs, nperseg=nperseg)
         return Data(psd, index=freq)
+
+    def data_acf(self, **kws):
+        """Estimate ACFs from data using Welch's method."""
+        dt = self.time_interval
+        fs = self.fft_span
+
+        psd = self.data_psd(**kws)
+        rho = 0.5 * np.fft.irfft(psd) * fs
+        return Data(rho, index=np.arange(len(rho))*dt)
