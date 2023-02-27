4bb185816de83883a11fe1225939c4892bb6006d
Neil Lu <neil.lu@anu.edu.au>
2023-02-27 11:54:08 +1100

    Implementing comments of PR

--- qnm_filter/utility.py
+++ qnm_filter/utility.py
@@ -1,58 +1,47 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+"""Useful functions for calculating and plotting data
 """
-Created on Mon Feb 20 17:55:14 2023
-
-@author: neil
-"""
-
+__all__ = ['parallel_compute', 'plotter']
 
 from joblib import Parallel, delayed
 import matplotlib.pyplot as pl
 import numpy as np
-import time
 
-def parallel_compute(function, x_arr, y_arr, kwargs = {}, show_time = True):
+def parallel_compute(function, M_arr, chi_arr, **kwargs):
     """Parallel computation of a function that takes 2 arguments 
     
     Arguments
     ---------
     function : function
         function that takes 2 args and an arbitrary number of kwargs
-    x_arr : array-like
+    M_arr : array-like
         array of the values of the first arg of the function
-    y_arr : array-like
+    chi_arr : array-like
         array of the values of the second arg of the function
     kwargs : dict
         dictionary of kwargs of the function
-    show_time : bool
-        whether to print time (in minutes) for the calculation
         
     Returns
     ---------
     reshaped_results : ndarray
         2d array of the results with shape (len(x_arr), len(y_arr))
     """
-    tic = time.time()
-    flatten_array = [(i,j) for i in x_arr for j in y_arr]
+    flatten_array = [(i,j) for i in M_arr for j in chi_arr]
     results = Parallel(-1)(delayed(function)(i, j, **kwargs) 
                      for i,j in flatten_array)
-    reshaped_results = np.reshape(results, (len(x_arr), len(y_arr))).T
-    toc = time.time()
-    if show_time:
-        calc_time = (toc-tic)/60
-        print("Calculation time = " + str(np.round(calc_time, 2)) + "mins")
+    reshaped_results = np.reshape(results, (len(M_arr), len(chi_arr))).T
     return reshaped_results
 
-def plotter(X, Y, results, credible_region=None, show = True, save_fig = False, **kwarg_dict):
-    pl.rc('figure', figsize=(6.1, 6.6))
+def plotter(X_grid, Y_grid, results, IMR_result = (np.nan, np.nan), credible_region=None, \
+            show = True, save_fig = False, fig_size = (6.1, 6.6), peak_time = 1126259462.4083147, \
+            **kwarg_dict):
+    pl.rc('figure', figsize=fig_size)
     fig, ax = pl.subplots()
-    contours = ax.contourf(X, Y, results, 20, cmap='Spectral',
+    contours = ax.contourf(X_grid, Y_grid, results, 20, cmap='Spectral',
                            origin='lower', alpha=1.0, linestyles='--')
-    ax.scatter(x=68.5, y=0.69, s=255, marker='+', 
+    ax.scatter(x=IMR_result[0], y=IMR_result[1], s=255, marker='+', 
                c='white', linewidths=4, label='IMR')
-    ax.axvline(x=68.5, c='k', ls = '--', alpha = 0.4, linewidth = 2)
-    ax.axhline(y=0.69, c='k', ls = '--', alpha = 0.4, linewidth = 2)
+    ax.axvline(x=IMR_result[0], c='k', ls = '--', alpha = 0.4, linewidth = 2)
+    ax.axhline(y=IMR_result[1], c='k', ls = '--', alpha = 0.4, linewidth = 2)
     ax.grid(visible = True, alpha = 0.5)
     
     if not credible_region == None:
@@ -69,8 +58,8 @@ def plotter(X, Y, results, credible_region=None, show = True, save_fig = False, 
     pl.ylabel(r'$\chi_f$', fontsize=13)
     ax.set_box_aspect(1)
 
-    time_str = str(np.round((kwarg_dict["t_init"] - 1126259462.4083147)*1e3, 2))
-    title = r'Filters = '+ str(kwarg_dict["model_list"]) + '  $\Delta t_0 = $' + time_str
+    time_str = str(np.round((kwarg_dict["t_init"] - peak_time)*1e3, 2))
+    title = r'Filters = '+ str(kwarg_dict["model_list"]) + '  $\Delta t_0 = $' + time_str + " ms"
     ax.set_title(title);
     
     if save_fig:
