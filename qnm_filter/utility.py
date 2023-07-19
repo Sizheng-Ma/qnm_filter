948ce8e10368f01e726d3f746e3bafefaf617e05
Sizheng Ma <sma@caltech.edu>
2023-07-19 10:32:53 -0700

    simplify find_probability_difference

--- qnm_filter/utility.py
+++ qnm_filter/utility.py
@@ -144,7 +144,7 @@ def time_to_index(self, index_spacing, tmin, tmax):
     return initial_offset, num_iteration
 
 
-def find_probability_difference(threshold, array2d, target_probability=0.9):
+def find_probability_difference(threshold, array2d):
     """Calculates the difference between the log probability of sampling array2d above the threshold and log target_probability
 
     Parameters
@@ -153,13 +153,11 @@ def find_probability_difference(threshold, array2d, target_probability=0.9):
         value to consider the probability of sampling above
     array2d : ndarray
         2D array of sampling log likelihood as a function of mass and spin
-    target_probability : float, optional
-        function returns 0 if the probability of sampling above the level = target_probability, by default 0.9
 
     Returns
     -------
     float
-        difference between the log probability of sampling array2d above the threshold and log target_probability
+        log probability of the given sampling array2d above the threshold
     """
     tot = logsumexp(array2d)
     region = array2d[array2d > threshold]
@@ -168,7 +166,7 @@ def find_probability_difference(threshold, array2d, target_probability=0.9):
     else:
         region_tot = logsumexp(region)
         prob = region_tot - tot
-    return prob - np.log(target_probability)
+    return prob
 
 
 def find_credible_region(array2d, num_cpu=-1, target_probability=0.9):
@@ -197,14 +195,13 @@ def find_credible_region(array2d, num_cpu=-1, target_probability=0.9):
     sorted_likelihood = np.sort(array2d.flatten())
 
     sorted_probability = Parallel(num_cpu)(
-        delayed(find_probability_difference)(i, array2d, target_probability)
-        for i in sorted_likelihood
+        delayed(find_probability_difference)(i, array2d) for i in sorted_likelihood
     )
     sorted_probability = np.array(sorted_probability)
 
     # interpolation is preferred when the sample density is insufficient
     interp_probability = interp1d(sorted_probability, sorted_likelihood)
-    return interp_probability(0)
+    return interp_probability(np.log(target_probability))
 
 
 def project_to_1d(array2d, delta_mass, delta_chi):
